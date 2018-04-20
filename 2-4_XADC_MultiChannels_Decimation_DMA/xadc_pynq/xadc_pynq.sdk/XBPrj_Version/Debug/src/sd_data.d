880cc2481632beccaeca3a4cd26c3699db6f21bd
xiamengxian <litexia@gmail.com>
2018-04-20 19:21:30 +0800

    Create a new branch 'ECJTU_Journal'

---
+++ 2-4_XADC_MultiChannels_Decimation_DMA/xadc_pynq/xadc_pynq.sdk/XBPrj_Version/Debug/src/sd_data.d
@@ -0,0 +1,102 @@
+src/sd_data.o src/sd_data.o: ../src/sd_data.c ../src/sd_data.h \
+ ../src/include.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xuartps_hw.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xuartlite.h \
+ ../src/debugLite.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/ff.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/integer.h \
+ ../../XBPrj_Version_bsp/ps7_cortexa9_0/include/ffconf.h
+
+../src/sd_data.h:
+
+../src/include.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/xuartlite.h:
+
+../src/debugLite.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/ff.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/integer.h:
+
+../../XBPrj_Version_bsp/ps7_cortexa9_0/include/ffconf.h:
